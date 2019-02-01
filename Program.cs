5997442eab58a852c89da52089e3b0eb24d1b536
Evan Taylor <taylorevan113@gmail.com>
2019-02-01 17:25:57 -0600

    Added EventWaitHandle, Manual/AutoResetEvent

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,6 +20,9 @@ namespace MCSA
             ObjectLocking();
             CancellationToken();
             Mutex();
+            Semaphore();
+            EventWaitHandle();
+            ManualResetEvent();
         }
 
 
@@ -312,7 +313,7 @@ namespace MCSA
         // A padding interval to make the output more orderly.
         private static int _padding;
 
-        public static void Main()
+        public static void Semaphore()
         {
             // Create a semaphore that can satisfy up to three concurrent requests. Use an initial 
             // count of zero, so that the entire semaphore count is initially owned by the main
@@ -344,8 +345,7 @@ namespace MCSA
         private static void Worker(object num)
         {
             // Each worker thread begins by requesting the semaphore.
-            Console.WriteLine("Thread {0} begins " +
-                "and waits for the semaphore.", num);
+            Console.WriteLine("Thread {0} begins and waits for the semaphore.", num);
             _pool.WaitOne();
 
             // A padding interval to make the output more orderly.
@@ -362,59 +362,335 @@ namespace MCSA
         }
 
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////
+        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+        // EventWaitHandle Example
+
+        // The EventWaitHandle used to demonstrate the difference
+        // between AutoReset and ManualReset synchronization events.
+        //
+        private static EventWaitHandle ewh;
+
+        // A counter to make sure all threads are started and
+        // blocked before any are released. A Long is used to show
+        // the use of the 64-bit Interlocked methods.
+        //
+        private static long threadCount = 0;
+
+        // An AutoReset event that allows the main thread to block
+        // until an exiting thread has decremented the count.
+        //
+        private static EventWaitHandle clearCount =
+            new EventWaitHandle(false, EventResetMode.AutoReset);
+
+        [MTAThread]
+        public static void EventWaitHandle()
+        {
+            // Create an AutoReset EventWaitHandle.
+            //
+            ewh = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+            // Create and start five numbered threads. Use the
+            // ParameterizedThreadStart delegate, so the thread
+            // number can be passed as an argument to the Start 
+            // method.
+            for (int i = 0; i <= 4; i++)
+            {
+                Thread t = new Thread(
+                    new ParameterizedThreadStart(ThreadProc)
+                );
+                t.Start(i);
+            }
+
+            // Wait until all the threads have started and blocked.
+            // When multiple threads use a 64-bit value on a 32-bit
+            // system, you must access the value through the
+            // Interlocked class to guarantee thread safety.
+            //
+            while (Interlocked.Read(ref threadCount) < 5)
+            {
+                Thread.Sleep(500);
+            }
+
+            // Release one thread each time the user presses ENTER,
+            // until all threads have been released.
+            //
+            while (Interlocked.Read(ref threadCount) > 0)
+            {
+                Console.WriteLine("Press ENTER to release a waiting thread.");
+                Console.ReadLine();
+
+                // SignalAndWait signals the EventWaitHandle, which
+                // releases exactly one thread before resetting, 
+                // because it was created with AutoReset mode. 
+                // SignalAndWait then blocks on clearCount, to 
+                // allow the signaled thread to decrement the count
+                // before looping again.
+                //
+                WaitHandle.SignalAndWait(ewh, clearCount);
+            }
+            Console.WriteLine();
+
+            // Create a ManualReset EventWaitHandle.
+            //
+            ewh = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+            // Create and start five more numbered threads.
+            //
+            for (int i = 0; i <= 4; i++)
+            {
+                Thread t = new Thread(
+                    new ParameterizedThreadStart(ThreadProc)
+                );
+                t.Start(i);
+            }
+
+            // Wait until all the threads have started and blocked.
+            //
+            while (Interlocked.Read(ref threadCount) < 5)
+            {
+                Thread.Sleep(500);
+            }
+
+            // Because the EventWaitHandle was created with
+            // ManualReset mode, signaling it releases all the
+            // waiting threads.
+            //
+            Console.WriteLine("Press ENTER to release the waiting threads.");
+            Console.ReadLine();
+            ewh.Set();
+
+        }
+
+        public static void ThreadProc(object data)
+        {
+            int index = (int)data;
+
+            Console.WriteLine("Thread {0} blocks.", data);
+            // Increment the count of blocked threads.
+            Interlocked.Increment(ref threadCount);
+
+            // Wait on the EventWaitHandle.
+            ewh.WaitOne();
+
+            Console.WriteLine("Thread {0} exits.", data);
+            // Decrement the count of blocked threads.
+            Interlocked.Decrement(ref threadCount);
+
+            // After signaling ewh, the main thread blocks on
+            // clearCount until the signaled thread has 
+            // decremented the count. Signal it now.
+            //
+            clearCount.Set();
+        }
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
         //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
+        // Auto Reset Event
+
+        private static AutoResetEvent event_1 = new AutoResetEvent(true);
+        private static AutoResetEvent event_2 = new AutoResetEvent(false);
+
+        static void AutoResetEvent()
+        {
+            Console.WriteLine("Press Enter to create three threads and start them.\r\n" +
+                              "The threads wait on AutoResetEvent #1, which was created\r\n" +
+                              "in the signaled state, so the first thread is released.\r\n" +
+                              "This puts AutoResetEvent #1 into the unsignaled state.");
+            Console.ReadLine();
+
+            for (int i = 1; i < 4; i++)
+            {
+                Thread t = new Thread((ParameterizedThreadStart)ThreadProc);
+                t.Name = "Thread_" + i;
+                t.Start();
+            }
+            Thread.Sleep(250);
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Press Enter to release another thread.");
+                Console.ReadLine();
+                event_1.Set();
+                Thread.Sleep(250);
+            }
+
+            Console.WriteLine("\r\nAll threads are now waiting on AutoResetEvent #2.");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Press Enter to release a thread.");
+                Console.ReadLine();
+                event_2.Set();
+                Thread.Sleep(250);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ThreadProcTwo()
+        {
+            string name = Thread.CurrentThread.Name;
+
+            Console.WriteLine("{0} waits on AutoResetEvent #1.", name);
+            event_1.WaitOne();
+            Console.WriteLine("{0} is released from AutoResetEvent #1.", name);
+
+            Console.WriteLine("{0} waits on AutoResetEvent #2.", name);
+            event_2.WaitOne();
+            Console.WriteLine("{0} is released from AutoResetEvent #2.", name);
+
+            Console.WriteLine("{0} ends.", name);
+        }
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
         //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
+        // Manual Reset Event
+
+        // mre is used to block and release threads manually. It is
+        // created in the unsignaled state.
+        private static ManualResetEvent mre = new ManualResetEvent(false);
+
+        static void ManualResetEvent()
+        {
+            Console.WriteLine("\nStart 3 named threads that block on a ManualResetEvent:\n");
+
+            for (int i = 0; i <= 2; i++)
+            {
+                Thread t = new Thread(ThreadProcThree);
+                t.Name = "Thread_" + i;
+                t.Start();
+            }
+
+            Thread.Sleep(500);
+            Console.WriteLine("\nWhen all three threads have started, press Enter to call Set()" +
+                              "\nto release all the threads.\n");
+            Console.ReadLine();
+
+            mre.Set();
+
+            Thread.Sleep(500);
+            Console.WriteLine("\nWhen a ManualResetEvent is signaled, threads that call WaitOne()" +
+                              "\ndo not block. Press Enter to show this.\n");
+            Console.ReadLine();
+
+            for (int i = 3; i <= 4; i++)
+            {
+                Thread t = new Thread(ThreadProcThree);
+                t.Name = "Thread_" + i;
+                t.Start();
+            }
+
+            Thread.Sleep(500);
+            Console.WriteLine("\nPress Enter to call Reset(), so that threads once again block" +
+                              "\nwhen they call WaitOne().\n");
+            Console.ReadLine();
+
+            mre.Reset();
+
+            // Start a thread that waits on the ManualResetEvent.
+            Thread t5 = new Thread(ThreadProcThree);
+            t5.Name = "Thread_5";
+            t5.Start();
+
+            Thread.Sleep(500);
+            Console.WriteLine("\nPress Enter to call Set() and conclude the demo.");
+            Console.ReadLine();
+
+            mre.Set();
+
+            // If you run this example in Visual Studio, uncomment the following line:
+            //Console.ReadLine();
+        }
 
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        private static void ThreadProcThree()
+        {
+            string name = Thread.CurrentThread.Name;
 
+            Console.WriteLine(name + " starts and calls mre.WaitOne()");
 
+            mre.WaitOne();
 
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
+            Console.WriteLine(name + " ends.");
+        }
     }
+
+        /* This example produces output similar to the following:
+
+            Start 3 named threads that block on a ManualResetEvent:
+
+            Thread_0 starts and calls mre.WaitOne()
+            Thread_1 starts and calls mre.WaitOne()
+            Thread_2 starts and calls mre.WaitOne()
+
+            When all three threads have started, press Enter to call Set()
+            to release all the threads.
+
+
+            Thread_2 ends.
+            Thread_0 ends.
+            Thread_1 ends.
+
+            When a ManualResetEvent is signaled, threads that call WaitOne()
+            do not block. Press Enter to show this.
+
+
+            Thread_3 starts and calls mre.WaitOne()
+            Thread_3 ends.
+            Thread_4 starts and calls mre.WaitOne()
+            Thread_4 ends.
+
+            Press Enter to call Reset(), so that threads once again block
+            when they call WaitOne().
+
+
+            Thread_5 starts and calls mre.WaitOne()
+
+            Press Enter to call Set() and conclude the demo.
+
+        Thread_5 ends.
+         */
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+}
 }
